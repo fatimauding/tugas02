3b8682060a14186ab45a7feeceeb82e06e2cf784
fatimauding <59597078+fatimauding@users.noreply.github.com>
2020-01-07 15:09:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -60,12 +60,12 @@
 			</tr>
 			<tr>
 				<td>
-					<img src="img/2.png" width="250px">
+					<img src="img/2.PNG" width="250px">
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<img src="img/1.png" width="250px">
+					<img src="img/1.PNG" width="250px">
 				</td>
 			</tr>
 			<tr>
